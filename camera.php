fe2ba7be9efcdfdfe1a32b77fda88748c80e83d1
moonsoo101 <moonsoo101@gmail.com>
2017-05-30 16:50:32 +0000

    first commit

---
+++ camera.php
@@ -0,0 +1,38 @@
+<?php
+$index = $_POST['index'];
+// $index =1;
+$sourceFile = 'image'.$index;
+// if($index!=='1')
+$last_line = system('raspistill -q 10 -t 1 -o '.$sourceFile.'.jpg' , $retval);
+$hostname = "ec2-13-124-33-214.ap-northeast-2.compute.amazonaws.com";
+$username = "ubuntu";
+$password = "leastb";
+$targetFile = '/var/www/html/darknet/data/'.$sourceFile.'.jpg';
+$connection = ssh2_connect($hostname, 22);
+ssh2_auth_password($connection, $username, $password);
+ssh2_scp_send($connection, $sourceFile.'.jpg', $targetFile, 0777);
+system('rm '.$sourceFile.'.jpg', $retval);
+$params = array ('filename' => $sourceFile);
+
+// Build Http query using params
+$query = http_build_query ($params);
+
+// Create Http context details
+$contextData = array (
+                'method' => 'POST',
+                'header' => "Connection: close\r\n".
+                            "Content-Length: ".strlen($query)."\r\n",
+                'content'=> $query );
+
+// Create context resource for our request
+$context = stream_context_create (array ( 'http' => $contextData ));
+
+// Read page rendered as result of your POST request
+$result =  file_get_contents (
+                  'http://ec2-13-124-33-214.ap-northeast-2.compute.amazonaws.com/darknet/Doyolo.php',  // page url
+                  false,
+                  $context);
+                  echo $result;
+
+echo "좆됨";
+?>
